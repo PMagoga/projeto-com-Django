d3a180c3e53cc7e1c5ab04ee7c97f7862e54df4d
Pablo <pmagoga@gmail.com>
2026-09-25 20:17:30 -0300

    adicionando variáveis de desenvolvimento

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 import os
 from pathlib import Path
-import dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv(os.path.join(BASE_DIR, ".env"))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
